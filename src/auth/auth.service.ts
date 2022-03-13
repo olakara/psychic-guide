a02ec962af4625f34f25ddf08ae08c3af14d7f13
Abdel Raoof <olakara@gmail.com>
2022-03-13 17:49:28 +0400

    register user and save to database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { DbContextService } from 'src/db-context/db-context.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private dbContext: DbContextService) {}
 
-  public signUp(dto: AuthDto) {
-    return { msg: 'I have signed up!' };
+  public async signUp(dto: AuthDto) {
+    // generate hash
+    const hash = await argon.hash(dto.password);
+    // save the user in db
+    const user = await this.dbContext.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // return user
+    return user;
   }
 
   public signIn() {
