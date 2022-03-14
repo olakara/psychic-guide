f43756f632bec10753c7465e015f06b75684c62e
Abdel Raoof <olakara@gmail.com>
2022-03-14 22:22:35 +0400

    condition for unique email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { DbContextService } from 'src/db-context/db-context.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private dbContext: DbContextService) {}
@@ -10,17 +11,25 @@ export class AuthService {
     // generate hash
     const hash = await argon.hash(dto.password);
     // save the user in finance department in db
-    const user = await this.dbContext.user.create({
-      data: {
-        email: dto.email,
-        hash,
-        departmentId: 'da482614-80b5-41f1-aba9-055586d0f8f7',
-      },
-    });
+    try {
+      const user = await this.dbContext.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          departmentId: 'da482614-80b5-41f1-aba9-055586d0f8f7',
+        },
+      });
 
-    delete user.hash;
-    // return user
-    return user;
+      delete user.hash;
+      // return user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already exists!');
+        }
+      }
+    }
   }
 
   public signIn() {
