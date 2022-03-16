a81386583e2dfb603c9c2cc0c868df26190dba7a
Abdel Raoof <olakara@gmail.com>
2022-03-16 19:52:09 +0400

    nest config and passport libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +7,14 @@ import { DepartmentModule } from './department/department.module';
 import { DbContextModule } from './db-context/db-context.module';
 
 @Module({
-  imports: [AuthModule, DepartmentModule, DbContextModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    DepartmentModule,
+    DbContextModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/db-context/db-context.service.ts
+++ src/db-context/db-context.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DbContextService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://puser:example@localhost:5432/nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
